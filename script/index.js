dd343045ae587d597eb7d176d7f84f52e2f87fd6
ARTALEAL <artaleal@yandex.ru>
2023-09-30 17:04:01 +0300

    dev: items effects

--- script/index.js
+++ script/index.js
@@ -23,4 +23,44 @@ if (ScrollTrigger.isTouch !== 1) {
       },
     }
   );
+
+  let itemsLeft = gsap.utils.toArray('.gallery__left-side .gallery__item');
+
+  itemsLeft.forEach((item) => {
+    gsap.fromTo(
+      item,
+      { opacity: 0, x: -100, y: -200 },
+      {
+        opacity: 1,
+        x: 0,
+        y: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-850',
+          end: '-100',
+          scrub: true,
+        },
+      }
+    );
+  });
+
+  let itemsRight = gsap.utils.toArray('.gallery__right-side .gallery__item');
+
+  itemsRight.forEach((item) => {
+    gsap.fromTo(
+      item,
+      { opacity: 0, x: 100, y: 300 },
+      {
+        opacity: 1,
+        x: 0,
+        y: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-850',
+          end: '-100',
+          scrub: true,
+        },
+      }
+    );
+  });
 }
