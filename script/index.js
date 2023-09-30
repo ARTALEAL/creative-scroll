b9c78e366bfbadac8a5ac1c6ca3b9cb31d40a1a9
ARTALEAL <artaleal@yandex.ru>
2023-09-30 16:30:13 +0300

    dev: header effects

--- script/index.js
+++ script/index.js
@@ -1,8 +1,26 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content',
-  smooth: 1.5,
-  effects: true,
-});
+console.log(ScrollTrigger.isTouch);
+
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true,
+  });
+
+  gsap.fromTo(
+    '.hero-section',
+    { opacity: 1 },
+    {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: '.hero-section',
+        start: 'center',
+        end: '900',
+        scrub: true,
+      },
+    }
+  );
+}
